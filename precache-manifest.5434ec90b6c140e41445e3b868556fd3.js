f47b59db3eb610b92e9d767e93e99c73f464bc33
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2019-12-11 14:32:29 +0000

    Deploy STEP-tw/gol-bsrushti to github.com/STEP-tw/gol-bsrushti.git:gh-pages

---
+++ precache-manifest.5434ec90b6c140e41445e3b868556fd3.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "23d148839f0d2a20acfa",
+    "url": "/gol-bsrushti/static/js/runtime~main.23d14883.js"
+  },
+  {
+    "revision": "ded81d5052c9a09107a9",
+    "url": "/gol-bsrushti/static/js/main.ded81d50.chunk.js"
+  },
+  {
+    "revision": "0cd6058fc3c24877ba89",
+    "url": "/gol-bsrushti/static/js/1.0cd6058f.chunk.js"
+  },
+  {
+    "revision": "ded81d5052c9a09107a9",
+    "url": "/gol-bsrushti/static/css/main.88f198f6.chunk.css"
+  },
+  {
+    "revision": "400c48adc412a57f08a32e860213b1c9",
+    "url": "/gol-bsrushti/index.html"
+  }
+];
